94afe390495c0e288cfcb2b29a22eeb4e5cc57ac
Caleb <kamenjuhkiiru@gmail.com>
2023-09-20 05:59:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+// code your solution here"
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (location = "go to the office") {
+    return `This Monday, I will ${location}.`
+}
+
+function wrapAdjective(flair = "*") {
+    return function (message = "special") {
+        return `You are ${flair}${message}${flair}!`
+    }
+}
+
+
